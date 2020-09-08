498ab988c05cd1332483b336b9124094ea8be72a
chriscorchado125 <chris.corchado125@gmail.com>
2020-09-07 22:35:51 -0400

    style tweaks across the board

--- ts/chriscorchado.ts
+++ ts/chriscorchado.ts
@@ -4,22 +4,21 @@ const API_BASE = "https://chriscorchado.com/drupal8";
 const MAX_ITEMS_PER_PAGE = 50;
 const SITE_SEARCH_ID = "searchSite";
 
-fetch("./includes/nav.html")
-  .then((response) => {
-    return response.text();
-  })
-  .then((data) => {
-    document.getElementById("navigation").innerHTML = data;
-  });
-
-fetch("./includes/footer.html")
-  .then((response) => {
-    return response.text();
-  })
-  .then((data) => {
-    document.getElementById("footer").innerHTML = data;
-  });
-
 window.onload = () => {
+  fetch("./includes/nav.html")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("navigation").innerHTML = data;
+    });
+
+  fetch("./includes/footer.html")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
   getPage(getCurrentPage());
 };
