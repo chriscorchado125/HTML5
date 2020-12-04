7bbe2646252a8da85c128996cb0e66c207f2f89b
chriscorchado125 <chris.corchado125@gmail.com>
2020-12-04 09:10:39 -0500

    test update

--- js/tests/chriscorchado.test.js
+++ js/tests/chriscorchado.test.js
@@ -1,18 +1,23 @@
+/**
+ * Get the current page name
+ * @return {string} - page name
+ */
 const getCurrentPage = () => {
-    let thisPage = window.location.pathname
-        .split("/")
-        .filter(function (pathnamePieces) {
-        return pathnamePieces.length;
-    })
-        .pop();
-    let pageName = "about";
-    if (thisPage)
-        pageName = thisPage.split(".")[0];
-    if (pageName == "index" || pageName == "nodejs")
-        pageName = "about";
-    return pageName;
-};
+    const thisPage = window.location.pathname
+      .split('/')
+      .filter(function (pathnamePieces) {
+        return pathnamePieces.length
+      })
+      .pop()
+
+    let pageName = ''
+    if (thisPage) pageName = thisPage.split('.')[0]
+
+    if (pageName === 'index' || pageName === 'html5' || !pageName) pageName = 'about'
+
+    return pageName
+  }
 
 test('get homepage', () => {
-    expect(getCurrentPage()).toMatch("about");
-});
+    expect(getCurrentPage()).toMatch("about")
+})
