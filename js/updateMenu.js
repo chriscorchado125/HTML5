43b2264e159d2b41fe8af3f258cd9229f1091d47
chriscorchado125 <chris.corchado125@gmail.com>
2020-12-05 11:59:30 -0500

    moved update menu function to its own file

---
+++ js/updateMenu.js
@@ -0,0 +1,48 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+export const updateMenuPages = (baseURL, currentPage, targetContainer) => __awaiter(void 0, void 0, void 0, function* () {
+    yield fetch(`${baseURL}/api/menu_items/main?_format=json`)
+        .then((resp) => {
+        return resp.ok ? resp.json() : Promise.reject(resp.statusText);
+    })
+        .then((pageData) => {
+        let pageName = '';
+        let pageLink = '';
+        let homepageStyle = '';
+        if (currentPage === 'about') {
+            homepageStyle = 'border: 1px dashed rgb(115, 153, 234)';
+        }
+        let generatedPageLinks = `<a href='index.html' class='navbar-brand' id='logo' style='${homepageStyle}'>
+        <img src='./images/chriscorchado-initials-logo.png' title='Home' alt='Home' id="logo-image">
+      </a>`;
+        for (const page in pageData) {
+            pageName = pageData[page].title;
+            if (pageName === 'Home' || pageName === 'About' || !pageData[page].enabled) {
+                continue;
+            }
+            let activeNavItem = '';
+            if (currentPage === pageName.toLowerCase()) {
+                activeNavItem = 'nav-item-active';
+            }
+            pageLink = pageName;
+            if (pageName === 'Companies')
+                pageName = 'History';
+            generatedPageLinks += `<a href='${pageLink.toLowerCase()}.html'
+        class='nav-item nav-link ${activeNavItem}'
+        title='${pageName}'
+        id='${pageName.toLowerCase()}-link'>${pageName}</a>`;
+        }
+        const targetContainerEL = document.getElementById(targetContainer);
+        targetContainerEL.innerHTML = generatedPageLinks;
+    })
+        .catch((error) => {
+        alert(`Sorry an error has occurred: ${error}`);
+    });
+});
--- ts/utilities.ts
+++ ts/utilities.ts
@@ -1,5 +1,3 @@
-// const {noRecordsFound} = require('./search');
-
 export const API_BASE = 'https://chriscorchado.com/drupal8'
 export const MAX_ITEMS_PER_PAGE = 50
 export const SITE_SEARCH_ID = 'search-site'
@@ -36,57 +34,6 @@ export const fadeIn = (el: any) => {
       requestAnimationFrame(fade)
     }
   })()
-}
-
-/**
- * Replace static navigation with data from the menu API
- * @param {string} currentPage - page name
- * @param {string} targetContainer - id of html container for the menu items
- */
-export const updateMenuPages = async (currentPage: string, targetContainer: string) => {
-  await fetch(`${API_BASE}/api/menu_items/main?_format=json`)
-    .then((resp) => {
-      return resp.ok ? resp.json() : Promise.reject(resp.statusText)
-    })
-    .then((pageData) => {
-      let pageName = ''
-      let pageLink = ''
-
-      let homepageStyle = ''
-      if (currentPage === 'about') {
-        homepageStyle = 'border: 1px dashed rgb(115, 153, 234)'
-      }
-
-      let generatedPageLinks = `<a href='index.html' class='navbar-brand' id='logo' style='${homepageStyle}'>
-        <img src='./images/chriscorchado-initials-logo.png' title='Home' alt='Home' id="logo-image">
-      </a>`
-
-      for (const page in pageData) {
-        pageName = pageData[page].title
-        if (pageName === 'Home' || pageName === 'About' || !pageData[page].enabled) {
-          continue
-        }
-
-        let activeNavItem = ''
-        if (currentPage === pageName.toLowerCase()) {
-          activeNavItem = 'nav-item-active'
-        }
-
-        pageLink = pageName // capture correct link name before pageName is updated
-        if (pageName === 'Companies') pageName = 'History'
-
-        generatedPageLinks += `<a href='${pageLink.toLowerCase()}.html'
-        class='nav-item nav-link ${activeNavItem}'
-        title='${pageName}'
-        id='${pageName.toLowerCase()}-link'>${pageName}</a>`
-      }
-
-      const targetContainerEL = document.getElementById(targetContainer) as HTMLElement
-      targetContainerEL.innerHTML = generatedPageLinks
-    })
-    .catch((error) => {
-      alert(`Sorry an error has occurred: ${error}`)
-    })
 }
 
 /**
