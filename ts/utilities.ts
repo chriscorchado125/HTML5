7ea0349733977bad2cbe5aec4e2e79fc49d6bdd9
chriscorchado125 <chris.corchado125@gmail.com>
2020-12-05 20:57:54 -0500

    lint

--- ts/utilities.ts
+++ ts/utilities.ts
@@ -143,10 +143,9 @@ export const setLoading = (loadingStatus: boolean) => {
 export const animateLogo = (logoID: string, animationID: string) => {
   const logoElement = document.getElementById(logoID) as HTMLElement
 
-  const checkExist = setInterval(function() {
+  const checkExist = setInterval (function() {
     if (logoElement) {
-
-      if (animationID){
+      if (animationID) {
         logoElement.setAttribute('src', `https://chriscorchado.com/images/chriscorchado-initials-logo-animated-${animationID}.gif`)
       } else {
         logoElement.setAttribute('src', 'https://chriscorchado.com/images/chriscorchado-initials-logo.png')
